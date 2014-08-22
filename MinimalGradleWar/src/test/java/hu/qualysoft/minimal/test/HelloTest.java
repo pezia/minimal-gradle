2b259e2dfdbe0ade7ebe0147bd1c8c91b0049c7f
Zsolt Petrik <pezia@pezia.hu>
2014-08-22 19:27:58 +0200

    Initial commit

---
+++ MinimalGradleWar/src/test/java/hu/qualysoft/minimal/test/HelloTest.java
@@ -0,0 +1,16 @@
+package hu.qualysoft.minimal.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Zsolt Petrik
+ */
+public class HelloTest {
+
+    @Test
+    public void testGetHello() {
+        Assert.assertTrue(true);
+    }
+}
