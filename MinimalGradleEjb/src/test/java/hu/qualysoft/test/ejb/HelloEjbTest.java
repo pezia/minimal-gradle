b75e464c284de3e4671ec2a94cbcf6a1af35a649
Zsolt Petrik <pezia@pezia.hu>
2014-08-22 19:44:22 +0200

    Added EJB module

---
+++ MinimalGradleEjb/src/test/java/hu/qualysoft/test/ejb/HelloEjbTest.java
@@ -0,0 +1,15 @@
+package hu.qualysoft.test.ejb;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Zsolt Petrik
+ */
+public class HelloEjbTest {
+    @Test
+    public void testHelloEjb() {
+        Assert.assertTrue(true);
+    }
+}
